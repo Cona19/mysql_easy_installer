000093530c72c57a19b903ab92f0e2ab947ba66a
Hyeonseok Oh <hyeonso5@gmail.com>
2017-07-28 11:57:31 +0900

    temp

--- server_install.sh
+++ server_install.sh
@@ -42,15 +42,16 @@ cp ../my.cnf ./
 
 ./bin/mysqld_safe &
 
-sleep 60
+sleep 120
 cd ../tpcc-mysql
+cd tpcc-mysql
 cd src
 make clean
 make
 cd ..
-mysqladmin -u root create tpcc10
-mysql -u root tpcc10 < create_table.sql 
-mysql -u root tpcc10 < add_fkey_idx.sql 
-./tpcc_load -h127.0.0.1 -P12943 -d tpcc10 -u root -p "" -w1
-./tpcc_start -h127.0.0.1 -P12943 -d tpcc10 -u root -p "" -w1 -c16 -r10 -l10
-mysqladmin -u root shutdown
+mysqladmin -u root create tpcc100
+mysql -u root tpcc100 < create_table.sql 
+mysql -u root tpcc100 < add_fkey_idx.sql 
+./tpcc_load -h127.0.0.1 -P12943 -d tpcc100 -u root -p "" -w100
+./tpcc_start -h127.0.0.1 -P12943 -d tpcc100 -u root -p "" -w100 -c48 -r10 -l300
+#mysqladmin -u root shutdown
